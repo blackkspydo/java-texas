5dd73c6a045a881f558d930d22a5e2db4a1e70ae
Kishor U <upadhyaya13kishor@gmail.com>
2023-05-29 08:03:27 +0545

    added changes

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         test();
+        test2(1, 4);
+        test2(6, 5);
+        String s = "Hello world!";
+        System.out.println(s);
+        System.out.println("Enter a string: ");
+        String s2 = sc.nextLine();
+        System.out.println(s2);
     }
 
     public static void test() {
         System.out.println("Hello world!");
+    }
+
+    static void test2(int a, int b) {
+        System.out.printf("a = %d, b = %d\n", a, b);
     }
 }
