bc39d237433faf3f2880b9e2dce6e575a6857729
Kishor U <upadhyaya13kishor@gmail.com>
2023-05-29 08:14:48 +0545

    added changes

--- test/src/Test.java
+++ test/src/Test.java
@@ -10,11 +10,39 @@ public class Test {
         System.out.println(s);
         System.out.println("Enter a string: ");
         String s2 = sc.nextLine();
-        String s3 = sc.nextLine();
-        String s4 = sc.nextLine();
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Enter a double: ");
+        double d = sc.nextDouble();
+        System.out.println("Enter a boolean: ");
+        boolean b = sc.nextBoolean();
+        System.out.println("Enter a char: ");
+        char c = sc.next().charAt(0);
+        System.out.println("Enter a long: ");
+        long l = sc.nextLong();
+        System.out.println("Enter a float: ");
+        float f = sc.nextFloat();
+        System.out.println("Enter a byte: ");
+        byte by = sc.nextByte();
+        System.out.println("Enter a short: ");
+        short sh = sc.nextShort();
+        System.out.println("Enter a string: ");
+        String s3 = sc.next();
+        System.out.println("Enter a string: ");
+        String s4 = sc.next();
+        System.out.println(s2);
+        System.out.println(n);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(by);
+        System.out.println(sh);
         System.out.println(s3);
         System.out.println(s4);
         System.out.println(s2);
+        sc.close();
     }
 
     public static void test() {
