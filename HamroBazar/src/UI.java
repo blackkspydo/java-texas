67eed0a7b73067b125e59a96c616048fdb1d5cc5
blackkspydo <upadhyaya13kishor@gmail.com>
2023-06-11 22:19:25 +0545

    aaded commands

--- HamroBazar/src/UI.java
+++ HamroBazar/src/UI.java
@@ -34,12 +34,23 @@ public class UI {
 
     public static void getFooter(Pages page) {
         String not_logged_in = """
-                1.Home         2.Signup
-                3.Exit
+                +---------------------------------------------------------------------------------+
+                Suggested commands:
+                login | signup | help | exit
+                +---------------------------------------------------------------------------------+
                 """;
         String home = """
-                1.Profile       2.Post Ad
-                3.View Ads      4.Logout
+                +---------------------------------------------------------------------------------+
+                Suggested commands:
+                goto profile | goto post | goto view | goto cart | help | exit
+                +---------------------------------------------------------------------------------+
+                """;
+        String profile = """
+                +---------------------------------------------------------------------------------_+
+                Suggested commands:
+                goto home | goto cart | help | exit
+                edit profile
+                +----------------------------------------------------------------------------------
                 """;
         switch (page) {
             case HOME_USER_NOT_LOGGED_IN -> System.out.println(not_logged_in);
@@ -53,10 +64,10 @@ public class UI {
         String[] commandArray = command.split(" ");
         int length = commandArray.length;
         String action = commandArray[0];
-        String page = length > 1 ? commandArray[1] : "";
+        String argument = length > 1 ? commandArray[1] : "";
         switch (action) {
             case "goto" -> {
-                switch (page) {
+                switch (argument) {
                     case "home" -> currentPage = Pages.HOME;
                     case "profile" -> currentPage = Pages.PROFILE;
                     case "post" -> currentPage = Pages.POST_PRODUCT;
@@ -67,12 +78,21 @@ public class UI {
             }
             case "back" -> currentPage = Pages.HOME;
             case "add" -> {
-                switch (page) {
+                switch (argument) {
                     case "product" -> System.out.println("Product added");
-
+                    case "cart" -> System.out.println("Product added to cart");
                     default -> System.out.println("Err: Invalid command");
                 }
             }
+            case "edit" -> {
+                switch (argument) {
+                    case "profile" -> System.out.println("Profile edited");
+                    case "product" -> System.out.println("Product edited");
+                    default -> System.out.println("Err: Invalid command");
+                }
+            }
+            case "next" -> System.out.println("Next page");
+            case "prev" -> System.out.println("Previous page");
             case "login" -> currentPage = Pages.LOGIN;
             case "signup" -> currentPage = Pages.REGISTER;
             case "help"-> currentPage = Pages.HELP;
